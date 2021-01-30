619a08d27f008353b7e93955970bed1f88e16d6a
mfbaltaza <araujogreydiego@gmail.com>
2021-01-30 19:18:57 -0400

    Eliminamos unas funciones de main.js y terminamos el resto de la web, faltan algunos ordenes en los divs del final pero ahí está. Funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,69 +49,6 @@
             });
         }
 
-        // == Testimonial Slider== //
-        if ($('.test-slider').length) {
-            var swiper = new Swiper('.test-slider', {
-                autoplay: {
-                    delay: 2500,
-                    disableOnInteraction: true,
-                },
-                speed: 1200,
-                loop: true,
-                pagination: {
-                    el: '.test-pagination',
-                    clickable: true
-                },
-                on: {
-                    slideChangeTransitionStart: function () {
-                        $('.testimonials .test-img, .testimonials h5, .testimonials span, .testimonials p').removeClass('aos-init').removeClass('aos-animate');
-                    },
-                    slideChangeTransitionEnd: function () {
-                        AOS.init();
-                    },
-                },
-            });
-
-            $(".test-slider").hover(function () {
-                (this).swiper.autoplay.stop();
-            }, function () {
-                (this).swiper.autoplay.start();
-            });
-        }
-
-        // == Clients Slider== //
-        if ($('.clients-slider').length) {
-            var swiper = new Swiper('.clients-slider', {
-                autoplay: {
-                    delay: 2500,
-                    disableOnInteraction: true,
-                },
-                speed: 900,
-                loop: true,
-                slidesPerView: 5,
-                breakpoints: {
-                    1200: {
-                        slidesPerView: 4
-                    },
-                    992: {
-                        slidesPerView: 3
-                    },
-                    576: {
-                        slidesPerView: 2
-                    },
-                    400: {
-                        slidesPerView: 1
-                    }
-                }
-            });
-
-            $(".clients-slider").hover(function () {
-                (this).swiper.autoplay.stop();
-            }, function () {
-                (this).swiper.autoplay.start();
-            });
-        }
-
         // == Light Gallery== //
         if ($('#lightgallery').length) {
             $("#lightgallery").lightGallery();
